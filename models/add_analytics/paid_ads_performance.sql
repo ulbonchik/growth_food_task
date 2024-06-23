ccac939138d629d4a389a6288786e7beb0e449c3
ulbonchik28@gmail.com <ulbonchik28@gmail.com>
2024-06-23 18:32:27 +0000

    1

--- models/add_analytics/paid_ads_performance.sql
+++ models/add_analytics/paid_ads_performance.sql
@@ -1,24 +1,41 @@
 WITH bing AS (
-    SELECT (
+    SELECT 
         channel,
         clicks,
         engagements,
         impressions,
         spend,
         total_conversions
-    )
     FROM {{ ref('bing_add_performance') }}
 ),
 facebook AS (
-    SELECT *
+    SELECT 
+        channel,
+        clicks,
+        engagements,
+        impressions,
+        spend,
+        total_conversions
     FROM {{ ref('facebook_add_performance') }}
 ),
 tiktok AS (
-    SELECT *
+    SELECT 
+        channel,
+        clicks,
+        engagements,
+        impressions,
+        spend,
+        total_conversions
     FROM {{ ref('tiktok_add_performance') }}
 ),
 twitter AS (
-    SELECT *
+    SELECT 
+        channel,
+        clicks,
+        engagements,
+        impressions,
+        spend,
+        total_conversions
     FROM {{ ref('twitter_add_performance') }}
 ),
 combined_data AS (
@@ -38,10 +55,19 @@ aggregated_data AS (
         SUM(impressions) AS total_impressions,
         SUM(spend) AS total_spend,
         SUM(total_conversions) AS total_conversions,
-        CASE WHEN SUM(total_conversions) = 0 THEN NULL ELSE SUM(spend) / SUM(total_conversions) END AS conversion_cost,
-        CASE WHEN SUM(engagements) = 0 THEN NULL ELSE SUM(spend) / SUM(engagements) END AS engagement_cost,
-        CASE WHEN SUM(clicks) = 0 THEN NULL ELSE SUM(spend) / SUM(clicks) END AS cpc
+        CASE 
+            WHEN SUM(total_conversions) = 0 THEN NULL 
+            ELSE SUM(spend) / SUM(total_conversions) 
+        END AS conversion_cost,
+        CASE 
+            WHEN SUM(engagements) = 0 THEN NULL 
+            ELSE SUM(spend) / SUM(engagements) 
+        END AS engagement_cost,
+        CASE 
+            WHEN SUM(clicks) = 0 THEN NULL 
+            ELSE SUM(spend) / SUM(clicks) 
+        END AS cpc
     FROM combined_data
-    GROUP BY channel)
-
-SELECT * from aggregated_data
+    GROUP BY channel
+)
+SELECT * FROM aggregated_data

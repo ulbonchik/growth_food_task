0fdafc658581dfaa1464cd8165b5be531fffeb7a
ulbonchik28@gmail.com <ulbonchik28@gmail.com>
2024-06-23 15:40:16 +0000

    1

--- models/staging/bing_add_performance.sql
+++ models/staging/bing_add_performance.sql
@@ -2,6 +2,8 @@ SELECT
     date,
     channel,
     clicks,
+    0 as engagements,
+    0 as impressions,
     spend,
     conv as conversions
 FROM {{ source('dbt_ulbonchik', 'src_ads_bing_all_data') }}
--- models/staging/twitter_add_performance.sql
+++ models/staging/twitter_add_performance.sql
@@ -3,6 +3,7 @@ date,
 channel,
 clicks,
 engagements,
+0 as impressions,
 spend,
 0 as conversions
 FROM {{ source('dbt_ulbonchik', 'src_promoted_tweets_twitter_all_data') }}
